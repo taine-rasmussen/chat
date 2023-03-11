473e464256f0a20d5b55b8c47721295f40903c70
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-11 19:54:41 +0000

    preview

--- client/src/components/customMessageForm/StandardMessageForm.jsx
+++ client/src/components/customMessageForm/StandardMessageForm.jsx
@@ -1,10 +1,31 @@
 import { useState } from "react";
-import MessageFormUI from "./MessageFormUI";
+import { XMarkIcon } from '@heroicons/react/24/solid';
+// import MessageFormUI from "./MessageFormUI";
 
 const StandardMessageForm = () => {
+  const [message, setMessage] = useState('');
+  const [attachment, setAttachment] = useState('');
+  const [preview, setPreview] = useState('')
+
+
   return (
-    <div>
-      StandardMessageForm
+    <div className='message-form-container'>
+      {preview && (
+        <div className='message-form-preview'>
+          <img
+            className='message-form-preview-image'
+            src={preview}
+            onLoad={() => URL.revokeObjectURL(preview)}
+          />
+          <XMarkIcon
+            className='message-form-icon-x'
+            onClick={() => {
+              setPreview('')
+              setAttachment('')
+            }}
+          />
+        </div>
+      )}
     </div>
   )
 }
