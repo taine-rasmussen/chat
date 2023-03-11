ece060da8527a6329c0b886cae72b23738aa2ba3
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-11 17:59:21 +0000

    nice

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
+import { BrowserRouter, Router, Route, Navigate, Routes } from 'react-router-dom';
 
 const App = () => {
 
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/chat' element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
