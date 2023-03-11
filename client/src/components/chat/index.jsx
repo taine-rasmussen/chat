7e2728053f0cb1eb4cc911b851272ea37a6331b7
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-11 18:13:34 +0000

    test user set up

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,6 +1,15 @@
 import { MultiChatSocket, useMultiChatLogic, MultiChatWindow } from "react-chat-engine-advanced"
 
 const Chat = () => {
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    'koiosreet',
+    '1234'
+  );
+
+
+
+
   return (
     <div>
       Cjat os wprlom
