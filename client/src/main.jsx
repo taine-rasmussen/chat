08b1edf06c903980059acadc88d1e25e7b493bf3
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-12 17:51:55 +0000

    ouch

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
-import { api } from '@state/api'
+import { api } from '@/state/api'
 
 export const store = configureStore({
   reducer: { [api.reducerPath]: api.reducer },
