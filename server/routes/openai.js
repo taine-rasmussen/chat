5af9630bdc9590ef9306002976e3d1661a2b9766
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-12 17:40:45 +0000

    hello?

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -9,6 +9,7 @@ const router = express.Router();
 router.post('/text', async (res, req) => {
   try {
     const { text, activeChatId } = req.body;
+    console.log('text:', text)
     res.status(200).json({ text })
   } catch (error) {
     console.log('error', error);
