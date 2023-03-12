458f4797915d47109d89729945e204e26f183b18
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-12 16:20:52 +0000

    app use

--- server/index.js
+++ server/index.js
@@ -4,3 +4,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json({ limit: '30mb' }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors())
