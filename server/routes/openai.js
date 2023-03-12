a3a400f6bee32e6b5c5b8da57bf09e6e91f1e36c
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-12 18:12:57 +0000

    a mess;

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -6,15 +6,15 @@ import { openai } from "../index.js";
 dotenv.config();
 const router = express.Router();
 
-router.post('/text', async (res, req) => {
+router.post("/text", async (req, res) => {
   try {
     const { text, activeChatId } = req.body;
-    console.log('text:', text)
-    res.status(200).json({ text })
+    console.log(text)
+    res.status(200).json({ text: text });
   } catch (error) {
-    console.log('error', error);
-    res.statusCode(500).json({ error: error.message })
+    console.error("error", error);
+    res.status(500).json({ error: error.message });
   }
-})
+});
 
 export default router
