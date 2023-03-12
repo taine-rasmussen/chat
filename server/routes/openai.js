cd9a748cd2a81b893d0dc16da20b07e12014a502
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-12 17:12:13 +0000

    fix import bug

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import axios from 'axios';
-import dotenv from 'dotenv';
-import { openai } from '../index.js'
+import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+import { openai } from "../index.js";
 
 dotenv.config();
 const router = express.Router();
