cc95601590e799af9b35f805b2a13c0c040c6d10
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-12 16:26:40 +0000

    openai config

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config();
 const app = express();
@@ -15,7 +16,13 @@ app.use(bodyParser.json({ limit: '30mb' }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors())
 
+// OPENAI CONFIG
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_AI_KEY,
+});
+const openai = new OpenAIApi(configuration)
+
 const PORT = process.env.PORT || 9000
 app.listen(PORT, () => {
   console.log('app listening at', PORT)
-})
+});
