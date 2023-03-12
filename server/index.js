69a63a8490ac02e143ed2fcb7a701125b30cd46f
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-12 16:23:18 +0000

    server starts wooooo

--- server/index.js
+++ server/index.js
@@ -14,3 +14,8 @@ app.use(morgan('common'));
 app.use(bodyParser.json({ limit: '30mb' }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors())
+
+const PORT = process.env.PORT || 9000
+app.listen(PORT, () => {
+  console.log('app listening at', PORT)
+})
