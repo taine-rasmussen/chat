fefed839fa04c55e70a23aef253918182493c687
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-14 21:58:45 +0000

    import api

--- client/src/components/login/index.jsx
+++ client/src/components/login/index.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import { usePostSignUpMutation, usePostLoginMutation } from '@/state/api'
 
 const Login = () => {
   return (
