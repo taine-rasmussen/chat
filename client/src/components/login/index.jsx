7a4418f352619fe06e75a73dc201b5a8364da2b5
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-14 22:01:36 +0000

    wrapper funcs and state

--- client/src/components/login/index.jsx
+++ client/src/components/login/index.jsx
@@ -2,9 +2,24 @@ import { useState, useEffect } from 'react';
 import { usePostSignUpMutation, usePostLoginMutation } from '@/state/api'
 
 const Login = () => {
+
+  const [isRegister, setIsRegister] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [triggerLogin] = usePostLoginMutation();
+  const [triggerSignUp] = usePostSignUpMutation();
+
+  const handleLogin = () => {
+    triggerLogin({ username, password })
+  };
+
+  const handleRegister = () => {
+    triggerSignUp({ username, password });
+  };
+
   return (
     <div>
-
+      Login
     </div>
   )
 }
