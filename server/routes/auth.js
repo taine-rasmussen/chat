04c9f1ca02a8441e11a39fdb7639f0a9d1ee3bc8
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-15 15:22:27 +0000

    send user info to chatengine api

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,4 +4,36 @@ import axios from "axios";
 const router = express.Router();
 
 
+router.post("/signup", async (req, res) => {
+  try {
+
+    const { username, password } = req.body;
+
+    const chatEngineResponse = await axios.post(
+      'https://api.chatengine.io/users/',
+      {
+        username: username,
+        secret: password
+      }
+    )
+
+    res.status(200).json({ text: response.data.choices[0].text });
+  } catch (error) {
+    console.error("error", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+router.post("/login", async (req, res) => {
+  try {
+
+    res.status(200).json({ text: response.data.choices[0].text });
+  } catch (error) {
+    console.error("error", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
 export default router;
