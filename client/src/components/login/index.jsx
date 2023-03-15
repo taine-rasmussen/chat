febed356611460f08f16f5b3ab62b697132f6122
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-15 15:09:12 +0000

    AHhhhhhhhhhhhhhhhhh

--- client/src/components/login/index.jsx
+++ client/src/components/login/index.jsx
@@ -27,28 +27,45 @@ const Login = ({ setUser, setSecret }) => {
   )
 
   return (
-    <div className='login-page'>
-      <div className='login-container'>
-        <h2 className='title'>ChatGPT App</h2>
-        <p className='register-change' onClick={() => setIsRegister(!isRegister)}>
-          {isRegister ? 'Already a user?' : 'Are you a new user?'}
+    <div className="login-page">
+      <div className="login-container">
+        <h2 className="title">CHATGPT APP</h2>
+        <p
+          className="register-change"
+          onClick={() => setIsRegister(!isRegister)}
+        >
+          {isRegister ? "Already a user?" : "Are you a new user?"}
         </p>
+
+        <div>
+          <input
+            className="login-input"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            className="login-input"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div className="login-actions">
+          {isRegister ? (
+            <button type="button" onClick={handleRegister}>
+              Register
+            </button>
+          ) : (
+            <button type="button" onClick={handleLogin}>
+              Login
+            </button>
+          )}
+        </div>
       </div>
-      <input
-        className='login-input'
-        type='text'
-        placeholder='Username'
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        className='login-input'
-        type='password'
-        placeholder='Password'
-        value={username}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      Login
     </div>
   )
 }
